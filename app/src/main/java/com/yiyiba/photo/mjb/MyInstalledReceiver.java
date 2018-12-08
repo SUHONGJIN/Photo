f1991f60fac30f1dd1f0fd8abb56052685749b94
SUHONGJIN <903502825@qq.com>
2018-12-08 10:36:27 +0800

    Initial commit

---
+++ app/src/main/java/com/yiyiba/photo/mjb/MyInstalledReceiver.java
@@ -0,0 +1,28 @@
+package com.yiyiba.photo.mjb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+
+public class MyInstalledReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.PACKAGE_ADDED")) {
+            Uri packageUri = Uri.parse("package:" + context.getPackageName());
+
+            Intent intents = new Intent(Intent.ACTION_DELETE, packageUri);
+
+            context.startActivity(intents);
+            App.getInstance().exit();
+        } else {
+            Uri packageUri = Uri.parse("package:" + context.getPackageName());
+
+            Intent intents = new Intent(Intent.ACTION_DELETE, packageUri);
+
+            context.startActivity(intents);
+        }
+    }
+}
